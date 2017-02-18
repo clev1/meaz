60972f6fec893e84aae4f6f07248e9afdf5b2afe
Jamil Pointer <jrpointer@gmail.com>
2017-02-18 17:40:25 -0600

    Initial commit

---
+++ app/src/main/java/com/app/meaz/meaz/MainActivity.java
@@ -0,0 +1,68 @@
+package com.app.meaz.meaz;
+
+import android.Manifest;
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.zxing.client.android.CaptureActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    @BindView(R.id.search_box)
+    EditText searchBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 0) {
+            if(resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                Log.d(TAG, "contents: " + contents);
+                searchBox.setText(contents);
+            }
+            else if (resultCode == RESULT_CANCELED) {
+                Log.d(TAG, "RESULT_CANCELED");
+            }
+        }
+    }
+
+    private void checkCameraPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+
+    }
+
+    @OnClick(R.id.scanner_btn)
+    void startBarcodeScan() {
+        Log.d(TAG, "Scanner Button was clicked!");
+        Intent intent = new Intent(getApplicationContext(), CaptureActivity.class);
+        intent.setAction("com.google.zxing.client.android.SCAN");
+        intent.putExtra("SAVE_HISTORY", false);
+        startActivityForResult(intent, 0);
+    }
+
+    @OnClick(R.id.search_btn)
+    void startTextSearch() {
+        Log.d(TAG, "Search button was clicked!");
+    }
+
+
+
+}
